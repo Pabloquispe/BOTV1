d53247745fde6b513cdc959fbef8c216a4e2b46e
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-01 06:08:46 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,7 @@
-import os
 from app import create_app
+import os
 
-# Obtén la configuración desde las variables de entorno
 config_name = os.getenv('FLASK_CONFIG') or 'default'
-
-# Crea la aplicación Flask usando la configuración especificada
 application = create_app(config_name)
 
 if __name__ == "__main__":
