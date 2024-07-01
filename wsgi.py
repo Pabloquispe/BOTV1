b26be5bd2085842f61373b345d8b68a9f689f1cb
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-01 06:57:07 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,5 @@
+# wsgi.py
 from app import create_app
-import os
 
 config_name = os.getenv('FLASK_CONFIG') or 'default'
 application = create_app(config_name)
-
-if __name__ == "__main__":
-    application.run()
