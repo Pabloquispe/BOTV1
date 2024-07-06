7139449690a358b56167196211b87e8094a07060
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-06 13:57:37 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import os
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG', 'config.ProductionConfig')
+config_name = os.getenv('FLASK_CONFIG', 'prod')
 app = create_app(config_name)
