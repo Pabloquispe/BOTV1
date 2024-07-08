ed96e8855e5428e44f79c3aa794ada095153034d
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-08 05:22:35 -0500

    Update config.py

--- config.py
+++ config.py
@@ -28,12 +28,12 @@ class Config:
     SESSION_COOKIE_NAME = 'my_session'
 
     # Configuración de correo electrónico
-    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.example.com')
-    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
-    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'false').lower() in ['true', 'on', '1']
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'your-email@example.com')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'your-email-password')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'false').lower() in ['true', 'on', '1']
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'false').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 class DevelopmentConfig(Config):
     """Configuración utilizada durante el desarrollo."""
